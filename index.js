110ad7b0213a1094d62e7e2db7373fe351b04129
creeperkafasi <creeperkafasiyt@gmail.com>
2021-07-31 23:31:10 +0300

    overlay sistemini ekledim

--- index.js
+++ index.js
@@ -11,19 +11,37 @@ let editorCanvas = document.querySelector("canvas");
 let editorCtx = editorCanvas.getContext("2d");
 
 
+if (!window.localStorage.getItem("drawMode"))
+    window.localStorage.setItem("drawMode", "squash");
+
+let drawMode = window.localStorage.getItem("drawMode");
+
+
 const overlayImage = document.createElement("img");
 overlayImage.src = "overlay.png";
 
-const loaderImage = document.createElement("img");
+const originalImage = document.createElement("img");
 
 function selectImage() {
     input.click();
 }
 
 reader.addEventListener("load", (res) => {
-    loaderImage.src = res.target.result;
+    originalImage.src = res.target.result;
     setTimeout(function() {
-            editorCtx.drawImage(loaderImage, 0, 0, 500, 500)
-
+            let drawSize;
+            switch (drawMode) {
+                case "squash":
+                    drawSize = [500, 500]
+                    break;
+                case "center":
+                    drawSize = [originalImage.width * 500 / originalImage.height, 500]
+                    break;
+                default:
+                    drawSize = [500, 500]
+                    break;
+            }
+            editorCtx.drawImage(originalImage, (drawSize[0] - 500) / -2, 0, drawSize[0], drawSize[1]);
+            editorCtx.drawImage(overlayImage, 0, 0, 500, 500);
         }, 200) // 200 ms bekletiyorum, lag fln oluyor bazen
 })
